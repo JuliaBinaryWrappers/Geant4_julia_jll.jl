0706b7bb9c7f75733c7e941b842015e11dee8e8f
jlbuild <juliabuildbot@gmail.com>
2023-04-04 02:43:07 +0000

    Geant4_julia_jll build 0.1.1+0

---
+++ src/Geant4_julia_jll.jl
@@ -0,0 +1,9 @@
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule Geant4_julia_jll
+using Base
+using Base: UUID
+import JLLWrappers
+
+JLLWrappers.@generate_main_file_header("Geant4_julia")
+JLLWrappers.@generate_main_file("Geant4_julia", UUID("e202cb52-5471-567a-9cb0-0ca301e27810"))
+end  # module Geant4_julia_jll
