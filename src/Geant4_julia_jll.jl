d631ba674812840692ebb637ae7f2e3e2a192939
jlbuild <juliabuildbot@gmail.com>
2025-12-09 20:06:25 +0000

    Geant4_julia_jll build 0.3.1+0

--- src/Geant4_julia_jll.jl
+++ src/Geant4_julia_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("Geant4_julia")
-JLLWrappers.@generate_main_file("Geant4_julia", UUID("e202cb52-5471-567a-9cb0-0ca301e27810"))
+JLLWrappers.@generate_main_file("Geant4_julia", Base.UUID("e202cb52-5471-567a-9cb0-0ca301e27810"))
 end  # module Geant4_julia_jll
